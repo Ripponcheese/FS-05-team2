43debda02cb98ae83db527e808f584673662dbf2
choi <choiyr1996@gmail.com>
2024-12-16 18:22:01 +0900

    init

---
+++ js문제집/최유리문제.js
@@ -0,0 +1,25 @@
+//
+const selectArry = [
+  { name: "김조순", gender: "male", value: 57 },
+  { name: "정하윤", gender: "male", value: 40 },
+  { name: "신진호", gender: "male", value: 76 },
+  { name: "최유리", gender: "female", value: 51 },
+  { name: "최수빈", gender: "female", value: 35 },
+];
+const selectArry2 = [
+  { name: "김상득", gender: "male", value: 15 },
+  { name: "오춘계", gender: "female", value: 48 },
+  { name: "김득배", gender: "male", value: 72 },
+  { name: "김햇살", gender: "female", value: 56 },
+  { name: "박춘식", gender: "female", value: 43 },
+];
+
+/*Q1. 위의 selectArry 에서 원본 배열을 그대로 두고 성별이 남자인 배열과 여자인 배열을 만들어라 */
+
+/*Q2. 위의 selectArry에서 원본 배열을 이름 내림 차순으로 정렬 시켜라. */
+
+/*Q3 selectArry에 name:"강태진" , gender : "male" 이라는 내용을 추가 하고  name이 김조순인 항목을 삭제 하여라*/
+
+/*Q4 selectArry와 selectArry2를 병합 하고 새로운 배열을 만들고 vqlue 값에 따라 정렬하여라 */
+
+/*Q5 두개의 배열을 병합한 새로운 배열에서 value의 값이 50이하는 제외한 새로운 배열을 만들어라 */
