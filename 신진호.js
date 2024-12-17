a6667855733f2a3507cecaf45349c5e96c59b89e
민지영 <kuck523@gmail.com>
2024-12-17 18:04:34 +0900

    fix: CSS, JS 수정 / aside-button id명 수정

--- 신진호.js
+++ 신진호.js
@@ -1,84 +1,71 @@
-//HI
-
-const acButton = document.querySelector('#acbutton');
-
+const acButton = document.querySelector("#acbutton");
 const profileContent = document.querySelector(".profile");
 const coreContent = document.querySelector(".core-attributes");
 const currentContent = document.querySelector(".current-objectives");
 const preferContent = document.querySelector(".preferred-arsenal");
 const behavioralContent = document.querySelector(".behavioral-matrix");
 const dashboard = document.querySelector(".dashboard");
-
 const accessContent = document.querySelector(".access-denied");
 
+document.querySelector("#profile-button").addEventListener("click", () => {
+  profileContent.style.display = "none";
+  coreContent.style.display = "none";
+  currentContent.style.display = "none";
+  preferContent.style.display = "none";
+  behavioralContent.style.display = "none";
+  dashboard.style.display = "none";
 
-document.querySelector('#profile').addEventListener('click', () => {
-    profileContent.style.display = "none";
-    coreContent.style.display = "none";
-    currentContent.style.display = "none";
-    preferContent.style.display = "none";
-    behavioralContent.style.display = "none";
-    dashboard.style.display = "none";
-
-    profileContent.style.display = "block";
+  profileContent.style.display = "block";
 });
 
-document.querySelector('#core').addEventListener('click', () => {
-    profileContent.style.display = "none";
-    coreContent.style.display = "none";
-    currentContent.style.display = "none";
-    preferContent.style.display = "none";
-    behavioralContent.style.display = "none";
-    dashboard.style.display = "none";
+document.querySelector("#core-button").addEventListener("click", () => {
+  profileContent.style.display = "none";
+  coreContent.style.display = "none";
+  currentContent.style.display = "none";
+  preferContent.style.display = "none";
+  behavioralContent.style.display = "none";
+  dashboard.style.display = "none";
 
-    coreContent.style.display = "block";
+  coreContent.style.display = "block";
 });
 
+document.querySelector("#current-button").addEventListener("click", () => {
+  profileContent.style.display = "none";
+  coreContent.style.display = "none";
+  currentContent.style.display = "none";
+  preferContent.style.display = "none";
+  behavioralContent.style.display = "none";
+  dashboard.style.display = "none";
 
-document.querySelector('#current').addEventListener('click', () => {
-    profileContent.style.display = "none";
-    coreContent.style.display = "none";
-    currentContent.style.display = "none";
-    preferContent.style.display = "none";
-    behavioralContent.style.display = "none";
-    dashboard.style.display = "none";
-
-    currentContent.style.display = "block";
+  currentContent.style.display = "block";
 });
 
+document.querySelector("#prefer-button").addEventListener("click", () => {
+  profileContent.style.display = "none";
+  coreContent.style.display = "none";
+  currentContent.style.display = "none";
+  preferContent.style.display = "none";
+  behavioralContent.style.display = "none";
+  dashboard.style.display = "none";
 
-document.querySelector('#prefer').addEventListener('click', () => {
-    profileContent.style.display = "none";
-    coreContent.style.display = "none";
-    currentContent.style.display = "none";
-    preferContent.style.display = "none";
-    behavioralContent.style.display = "none";
-    dashboard.style.display = "none";
-
-    preferContent.style.display = "block";
+  preferContent.style.display = "block";
 });
 
+document.querySelector("#behavioral-button").addEventListener("click", () => {
+  profileContent.style.display = "none";
+  coreContent.style.display = "none";
+  currentContent.style.display = "none";
+  preferContent.style.display = "none";
+  behavioralContent.style.display = "none";
+  dashboard.style.display = "none";
 
-document.querySelector('#behavioral').addEventListener('click', () => {
-    profileContent.style.display = "none";
-    coreContent.style.display = "none";
-    currentContent.style.display = "none";
-    preferContent.style.display = "none";
-    behavioralContent.style.display = "none";
-    dashboard.style.display = "none";
-
-    behavioralContent.style.display = "block";
-
+  behavioralContent.style.display = "block";
 });
 
+document.querySelector(".content-button").addEventListener("click", () => {
+  accessContent.style.display = "block";
+});
 
-
-
-
-document.querySelector('.content-button').addEventListener('click', () => {
-    accessContent.style.display = "block";
-})
-
-document.querySelector('.acbutton').addEventListener('click', () => {
-    accessContent.style.display = "none";
-})
+document.querySelector(".acbutton").addEventListener("click", () => {
+  accessContent.style.display = "none";
+});
