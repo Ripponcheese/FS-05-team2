82d82a556f3ef0ffb61c701f97ceba0cecd546e6
choi <choiyr1996@gmail.com>
2024-12-17 09:37:03 +0900

    최유리문제ver1

--- js문제집/최유리문제.js
+++ js문제집/최유리문제.js
@@ -1,25 +1,12 @@
-//
-const selectArry = [
-  { name: "김조순", gender: "male", value: 57 },
-  { name: "정하윤", gender: "male", value: 40 },
-  { name: "신진호", gender: "male", value: 76 },
-  { name: "최유리", gender: "female", value: 51 },
-  { name: "최수빈", gender: "female", value: 35 },
-];
-const selectArry2 = [
-  { name: "김상득", gender: "male", value: 15 },
-  { name: "오춘계", gender: "female", value: 48 },
-  { name: "김득배", gender: "male", value: 72 },
-  { name: "김햇살", gender: "female", value: 56 },
-  { name: "박춘식", gender: "female", value: 43 },
-];
+function mathAge(number){
 
-/*Q1. 위의 selectArry 에서 원본 배열을 그대로 두고 성별이 남자인 배열과 여자인 배열을 만들어라 */
 
-/*Q2. 위의 selectArry에서 원본 배열을 이름 내림 차순으로 정렬 시켜라. */
 
-/*Q3 selectArry에 name:"강태진" , gender : "male" 이라는 내용을 추가 하고  name이 김조순인 항목을 삭제 하여라*/
+}
 
-/*Q4 selectArry와 selectArry2를 병합 하고 새로운 배열을 만들고 vqlue 값에 따라 정렬하여라 */
 
-/*Q5 두개의 배열을 병합한 새로운 배열에서 value의 값이 50이하는 제외한 새로운 배열을 만들어라 */
+// Q1. 나이를 계산해주는 mathAge함수를 만드시오
+// Q2. 파라미터 number는 6자리만 받도록 조건을 설정하시오
+// Q3. 나이가 80세가 넘으면 년도 전체 4자리를 추가로 받아서 사용하는 리턴함수를 만드시오
+// Q4. 입력받은 리턴함수의 값으로 mathAge 파라미터의 값을 변경해서 나이를 계산하시오
+// Q5. 최종결과값은 '#세 입니다.'로 출력하시오오
